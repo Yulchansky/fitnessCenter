75435b26a0086ed4db8c2aef76c43002b41c32a0
Yulchansky <103851273+Yulchansky@users.noreply.github.com>
2023-04-10 23:35:10 +0300

    Add files via upload

---
+++ StaticsCont.java
@@ -0,0 +1,12 @@
+package com.fitness.controller;
+
+import com.fitness.controller.main.Attributes;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+public class StaticsCont extends Attributes {
+
+}
